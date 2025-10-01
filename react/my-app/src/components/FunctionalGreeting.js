65253649afcb317b5556c61950d55165d7cb86fb
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-10-01 15:02:46 +0100

    Fix: added react/react as normal folder

---
+++ react/my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+//function FunctionalGreeting() {
+//  return <h1>Hello from REACT!</h1>;
+//}
+
+const FunctionalGreeting = () => <h1>Hello from REACT arrow function!</h1>;
+
+export default FunctionalGreeting;
