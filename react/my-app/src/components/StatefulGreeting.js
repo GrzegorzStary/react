19a211e4b6bb7278d8eecd57c408fe40bd85ea67
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-10-01 16:48:25 +0100

    UPDATE: State code in react 3 rules

--- react/my-app/src/components/StatefulGreeting.js
+++ react/my-app/src/components/StatefulGreeting.js
@@ -1,9 +1,55 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
-    render() {
-        return <h1>Hello {this.props.greeting} {this.props.name}</h1>
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+            count: 0,
+        };
     }
-}
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log('previous state', prevState);
+            console.log('previous props', prevProps);
+            return {
+                introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
+            });
+    
+        }
+    
+        increment() {
+            this.setState((prevState, prevProps) => {
+                console.log('previous state', prevState);
+                console.log('previous props', prevProps);
+                return { count: prevState.count + 1 }
+            });
+
+            console.log(this.state.count);
+        }
+
+        incrementFive() {
+            this.increment();
+            this.increment();
+            this.increment();
+            this.increment();
+            this.increment();
+        }
+
+        render() {
+            return (
+                <div>
+                    <h1>{this.state.introduction} {this.props.greeting} {this.props.name}</h1>
+                    <button onClick={() => this.handleClick()}> {this.state.buttonText}</button>
+                    <button onClick={() => this.increment()}> Increment by: {this.state.count}</button>
+                </div>
+            )
+        }
+    }
 
 export default StatefulGreeting;
