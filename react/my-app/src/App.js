8f70fee75e9f2754c72b3ef5fd948a261dba8926
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-10-20 12:31:57 +0800

    Event Binding 4 ways, arrow

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className="App">
-      <EventsClass />
-      <EventsFunctional />
+      <EventBinding />
     </div>
   );
 }
